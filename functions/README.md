9942d16265e2fcabb2876b7d23ebe948dd84682d
Vikram Bhogiraju <vikram.bhogiraju@gmail.com>
2021-04-26 02:09:47 +0530

    Added: functions/default_params.py Updated: functions/README.md

--- functions/README.md
+++ functions/README.md
@@ -3,4 +3,5 @@
 ## Introduction
 
 * [fib.py](/functions/fib.py) : introduces basic function syntax with a parameter, docstring and return statement.
-* [ask_ok.py](/functions/ask_ok.py) : demonstrates use of default argument values
+* [ask_ok.py](/functions/ask_ok.py) : demonstrate use of default argument values
+* [default_params.py](/functions/default_params.py): demonstrate behavior of default params to functions
---
+++ functions/default_parms.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Functions that demonstrate behavior of default params to functions"""
+
+# Default param can be a value of a variable but is evaluated at the time of definition
+i = 5
+def check_default_arg_value(arg=i):  # evaluates to def f(arg=5):
+    print('arg in check_default_arg_value() = ', arg)
+
+print('i =', i) # prints i = 5
+check_default_arg_value()  # prints 5 as default param is 5
+
+i = 6
+print('i =', i) # prints i = 6
+check_default_arg_value()  # prints 5 again as changes to i after definition of f has no effect 
+                           # on the default param of f
+
+# Also, default value is evaluated only once. So, when the default is a mutable object 
+# such as list, dict or instances of classes, the earlier values take effect
+
+# Function accumulate_arg() below accumulates the args passed to it
+def accumulate_arg(a, L=[]):
+    L.append(a)
+    return L
+
+print('accumulate_arg(1):', accumulate_arg(1)) # prints [1]
+print('accumulate_arg(2):', accumulate_arg(2)) # prints [1, 2]
+print('accumulate_arg(3):', accumulate_arg(3)) # prints [1, 2, 3]
+
