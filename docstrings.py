16a59c077d090d20b7e423b58731e58cb34884bd
Vikram Bhogiraju <vikram.bhogiraju@gmail.com>
2021-05-11 23:26:57 +0530

    Add: functions/     arbitrary_args.py     lambda.py     unpacking_args.py ./docstrings.py

---
+++ docstrings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def my_function():
+    """Do nothing, but document it.
+
+    No, really, it doesn't do anything.
+    """
+    pass
+
+print(my_function.__doc__)
---
+++ functions/lambda.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def make_incrementor(n):
+    return lambda x: x + n
+
+f = make_incrementor(42) # Define f as incrementor by 42
+print(f(0)) # prints 42
+print(f(10)) # prints 52
+
+pairs = [(1, "one"), (2, "two"), (3, "three"), (4, "four")]
+pairs.sort(key=lambda pair:pair[1]) # orders by second element of each list item
+print(pairs) 
